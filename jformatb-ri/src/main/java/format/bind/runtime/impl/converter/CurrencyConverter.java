1b3eac39e1f7eaac49665695c2e110e88394aa82
Yannick Ebongue <yannickebongue@gmail.com>
2025-11-06 22:47:16 +0100

    Add currency format by numeric code

--- jformatb-ri/src/main/java/format/bind/runtime/impl/converter/CurrencyConverter.java
+++ jformatb-ri/src/main/java/format/bind/runtime/impl/converter/CurrencyConverter.java
@@ -15,32 +15,58 @@
  */
 package format.bind.runtime.impl.converter;
 
+import java.util.Comparator;
 import java.util.Currency;
+import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
 
 import format.bind.FormatFieldDescriptor;
+import format.bind.annotation.FormatField.Type;
 import format.bind.converter.FieldConversionException;
 import format.bind.converter.FieldConverter;
 
 final class CurrencyConverter implements FieldConverter<Currency> {
 
+	private static Map<Integer, Currency> numericCodes = Currency.getAvailableCurrencies().stream()
+			.sorted(Comparator.comparing(Currency::getNumericCode))
+			.filter(currency -> currency.getNumericCode() > 0)
+			.collect(Collectors.toMap(Currency::getNumericCode, Function.identity(), (u, v) -> u));
+
+	private static Currency getInstance(int numericCode) {
+		return Optional.ofNullable(numericCodes.get(numericCode))
+				.orElseThrow(() -> new IllegalArgumentException(String.format("No ISO 4217 currency found with numeric code %d", numericCode)));
+	}
+
+	private static Currency getInstance(String currencyCode) {
+		return Currency.getInstance(currencyCode);
+	}
+
 	@Override
 	public String format(FormatFieldDescriptor descriptor, Currency value) throws FieldConversionException {
-		try {
+		if (descriptor.type() == Type.NUMERIC) {
+			return StringUtils.leftPad(Optional.ofNullable(value)
+					.map(Currency::getNumericCode)
+					.map(String::valueOf)
+					.orElseGet(descriptor::placeholder), descriptor.length(), "0");
+		} else {
 			return StringUtils.rightPad(Optional.ofNullable(value)
-					.map(Currency::toString)
+					.map(Currency::getCurrencyCode)
 					.orElseGet(descriptor::placeholder), descriptor.length());
-		} catch (Exception e) {
-			throw FieldConverters.formatFieldConversionException(descriptor, value, e);
 		}
 	}
 
 	@Override
 	public Currency parse(FormatFieldDescriptor descriptor, String source) throws FieldConversionException {
 		try {
-			return Currency.getInstance(StringUtils.trimToEmpty(source));
+			if (descriptor.type() == Type.NUMERIC) {
+				return getInstance(Integer.valueOf(source));
+			} else {
+				return getInstance(StringUtils.trimToEmpty(source));
+			}
 		} catch (Exception e) {
 			throw FieldConverters.parseFieldConversionException(descriptor, source, e);
 		}
---
+++ jformatb-ri/src/test/java/format/bind/runtime/impl/converter/CurrencyConverterTest.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2024 jFormat-B
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package format.bind.runtime.impl.converter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import java.util.Comparator;
+import java.util.Currency;
+import java.util.Locale;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
+
+import format.bind.FormatFieldDescriptor;
+import format.bind.annotation.FormatField.Type;
+import format.bind.converter.FieldConversionException;
+
+class CurrencyConverterTest extends AbstractConverterTest<Currency> {
+
+	public CurrencyConverterTest() {
+		super(Currency.class);
+	}
+
+	@Test
+	void formatCurrency() {
+		FormatFieldDescriptor descriptor = fieldDescriptorBuilder()
+				.length(3)
+				.locale("en-US")
+				.build();
+		Currency currency = Currency.getInstance(Locale.forLanguageTag(descriptor.locale()));
+		String actual = converter.format(descriptor, currency);
+		assertThat(actual).isEqualTo("USD");
+	}
+
+	@Test
+	void parseCurrency() {
+		FormatFieldDescriptor descriptor = fieldDescriptorBuilder()
+				.length(3)
+				.build();
+		Currency expected = Currency.getInstance(Locale.getDefault());
+		Currency actual = converter.parse(descriptor, StringUtils.rightPad(expected.getCurrencyCode(), descriptor.length()));
+		assertThat(actual).usingComparator(Comparator.comparing(Currency::getCurrencyCode)).isEqualTo(expected);
+	}
+
+	@Test
+	void formatCurrencyNumericCode() {
+		FormatFieldDescriptor descriptor = fieldDescriptorBuilder()
+				.length(3)
+				.type(Type.NUMERIC)
+				.locale("en-US")
+				.build();
+		Currency currency = Currency.getInstance(Locale.forLanguageTag(descriptor.locale()));
+		String actual = converter.format(descriptor, currency);
+		assertThat(actual).isEqualTo("840");
+	}
+
+	@Test
+	void parseCurrencyNumericCode() {
+		FormatFieldDescriptor descriptor = fieldDescriptorBuilder()
+				.length(3)
+				.type(Type.NUMERIC)
+				.build();
+		Currency expected = Currency.getInstance(Locale.getDefault());
+		Currency actual = converter.parse(descriptor, StringUtils.leftPad(String.valueOf(expected.getNumericCode()), descriptor.length(), "0"));
+		assertThat(actual).usingComparator(Comparator.comparing(Currency::getNumericCode)).isEqualTo(expected);
+	}
+
+	@Test
+	void parseInvalidCurrencyCode() {
+		FormatFieldDescriptor descriptor = fieldDescriptorBuilder()
+				.length(3)
+				.build();
+		assertThatExceptionOfType(FieldConversionException.class)
+				.isThrownBy(() -> converter.parse(descriptor, "AAA"))
+				.withCauseInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	void parseInvalidNumericCode() {
+		FormatFieldDescriptor descriptor = fieldDescriptorBuilder()
+				.length(3)
+				.type(Type.NUMERIC)
+				.build();
+		assertThatExceptionOfType(FieldConversionException.class)
+				.isThrownBy(() -> converter.parse(descriptor, "001"))
+				.withCauseInstanceOf(IllegalArgumentException.class);
+	}
+
+}
